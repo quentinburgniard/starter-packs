659ffcbca4a6d92d4dfd108813ab7a5bef14d88b
Quentin Burgniard <qburgniard@wideagency.com>
2024-02-13 10:54:27 +0100

    feat(html): add webpack

--- express-vue/vite/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import Component from './Component.vue'
-//import router from './router'
-
-const component = createApp(Component)
-
-//app.use(router)
-
-component.mount('#component')
