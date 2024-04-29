540c8d0e22432dbc5d65c363d7a7512dd02eea0a
Quentin Burgniard <qburgniard@wideagency.com>
2024-04-29 14:05:54 +0200

    feat(drupal): add theme

--- drupal/sites/default/settings.php
+++ drupal/sites/default/settings.php
@@ -263,7 +263,7 @@ $databases['default']['default'] = [
  * directory in the public files path. The setting below allows you to set
  * its location.
  */
-# $settings['config_sync_directory'] = '/directory/outside/webroot';
+$settings['config_sync_directory'] = '/opt/drupal';
 
 /**
  * Settings:
---
+++ drupal/theme/js/theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.theme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
